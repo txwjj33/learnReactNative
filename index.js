eef109bb9af4cb06045cd8341c08c36ec5fd6fa0
tanxinwen <tanxinwen@xiaoyezi.com>
2020-03-30 19:00:50 +0800

    add stackNavigator.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import Main from './src/moviesSearch';
+import Main from './src/navigation/stackNavigator';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => Main);
